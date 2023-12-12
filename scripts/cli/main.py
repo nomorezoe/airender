7f04a0d68a1fde53f5f41e91ff437356a7489f66
nomorezoe <zoewangwork@gmail.com>
2023-12-12 23:24:10 +0800

    d

--- scripts/cli/main.py
+++ scripts/cli/main.py
@@ -409,7 +409,7 @@ def load_styles():
 
 def main(image_id, use_inpaint, use_depth_map, batch_count, prompt, control_net_model, model_id, scheduler_type, lora_id, cfg, clip_skip, sampler_steps, vae, inpaint_strength, use_style, style, depth_strength, pose_strength):
     
-    prompt = get_styled_prompt(style, prompt)
+    prompt = get_styled_prompt(style, prompt) + "{masterpiece, high quality, high resolution, 4K, HDR}"
     n_prompt = "(worst quality, low quality, normal quality, lowres, low details, oversaturated, undersaturated, overexposed, underexposed, grayscale, bw, bad photo, bad photography, bad art:1.4), (watermark, signature, text font, username, error, logo, words, letters, digits, autograph, trademark, name:1.2), (blur, blurry, grainy), morbid, ugly, asymmetrical, mutated malformed, mutilated, poorly lit, bad shadow, draft, cropped, out of frame, cut off, censored, jpeg artifacts, out of focus, glitch, duplicate, (airbrushed, cartoon, anime, semi-realistic, cgi, render, blender, digital art, manga, amateur:1.3), (3D ,3D Game, 3D Game Scene, 3D Character:1.1), (bad hands, bad anatomy, bad body, bad face, bad teeth, bad arms, bad legs, deformities:1.3) horror"
     n_prompt = get_styled_neg_prompt(style) + n_prompt
 
